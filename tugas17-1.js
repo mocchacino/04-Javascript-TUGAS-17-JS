c440500602fc70d2999fc0bff0b428e80ddbcaa1
wiladhianty <wiladhianty@gmail.com>
2021-06-30 13:54:06 +0700

    tambah tugas

---
+++ tugas17-1.js
@@ -0,0 +1,10 @@
+var upLow1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk."
+var upLow2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual."
+
+// mengubah variabel upLow1 menjadi uppercase dan lowercase
+console.log(upLow1.toLowerCase());
+console.log(upLow1.toUpperCase())
+
+// mengubah variabel upLow1 menjadi uppercase dan lowercase
+console.log(upLow2.toLowerCase());
+console.log(upLow2.toUpperCase())
